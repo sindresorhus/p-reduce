615f5f266a7b4d51b55b76477325f04bcac81ec0
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-11 20:38:04 +0100

    Require Node.js 8, add TypeScript definition

--- test.js
+++ test.js
@@ -1,24 +1,24 @@
 import test from 'ava';
 import delay from 'delay';
-import m from './';
+import pReduce from '.';
 
 test('main', async t => {
 	const f = [
 		Promise.resolve(3),
-		delay(50).then(() => 2),
+		delay(50, {value: 2}),
 		5
 	];
 
-	t.is(await m(f, (a, b) => Promise.resolve(a + b), 0), 10);
+	t.is(await pReduce(f, async (a, b) => a + b, 0), 10);
 });
 
 test('rejects', async t => {
 	const f = [
 		Promise.resolve(3),
-		delay(50).then(() => Promise.reject(new Error('foo')))
+		delay.reject(50, {value: new Error('foo')})
 	];
 
-	await t.throws(m(f, (a, b) => a + b, 0), 'foo');
+	await t.throwsAsync(pReduce(f, (a, b) => a + b, 0), 'foo');
 });
 
 test('reducer throws', async t => {
@@ -27,11 +27,11 @@ test('reducer throws', async t => {
 		Promise.resolve(3)
 	];
 
-	await t.throws(m(f, () => {
+	await t.throwsAsync(pReduce(f, () => {
 		throw new Error('foo');
 	}), 'foo');
 });
 
 test('handles empty iterable', async t => {
-	t.deepEqual(await m([], () => {}, 0), 0);
+	t.deepEqual(await pReduce([], () => {}, 0), 0);
 });
